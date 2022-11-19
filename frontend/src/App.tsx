b5ce8253a6203f62942c63353c0a58c253ded6c0
Kaike <kzuanetti53@gmail.com>
2022-11-19 19:45:02 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import NotificationButton from "./components/NotificationButton";
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard"
+import { ToastContainer } from "react-toastify";
 export default function App() {
 
   return (
   <div>
+    <ToastContainer />
     <Header />
     <main>
       <section id="sales">
